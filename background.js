1760efd10fc879586cb60392c9492a0ee86edd2f
Andrea <siizx@live.it>
2024-05-11 21:56:05 +0200

    FUNZIONAAAA v1.0

--- background.js
+++ background.js
@@ -1,19 +1,23 @@
-function updateTabTitles() {
-  console.log("Background script running!"); // For debugging purposes
+let tabTitles = {};
 
+function updateTabTitles() {
   chrome.tabs.query({}, function(tabs) {
     for (let i = 0; i < tabs.length; i++) {
       const tab = tabs[i];
-      const newTitle = `${i + 1} ${tab.title}`;
-      chrome.tabs.update(tab.id, { active: true, title: newTitle });
+      if (!tabTitles[tab.id]) {
+        tabTitles[tab.id] = tab.title.replace(/^[0-9]+ /, ''); // Remove existing tab number if any
+      }
+      chrome.tabs.executeScript(tab.id, {
+        code: 'document.title = `' + (i + 1) + ' ' + tabTitles[tab.id] + '`;'
+      });
     }
   });
 }
 
-
 chrome.tabs.onCreated.addListener(updateTabTitles);
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
   if (changeInfo.title) {
+    tabTitles[tabId] = changeInfo.title.replace(/^[0-9]+ /, ''); // Remove existing tab number if any
     updateTabTitles();
   }
 });
